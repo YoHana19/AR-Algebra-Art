5f51e8e685097f54f656ce5346e6a3f2fbe8c93a
YoHana19 <yohanashima@mail.com>
2017-12-02 15:39:52 +0900

    Increment cube move

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour {
+
+	private float currentSize = 0.0f;
+	public float maxSize;
+	private float currentPosition = 0.0f;
+	private float mediatedPosition;
+	private float finalPositon;
+
+	// Use this for initialization
+	void Start () {
+		mediatedPosition = -0.5f;
+		currentPosition += mediatedPosition;
+		this.transform.position = new Vector3 (currentPosition, this.transform.position.y, this.transform.position.z);
+		finalPositon = maxSize / 2 + mediatedPosition;
+		this.transform.localScale = new Vector3(currentSize, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentSize < maxSize) {
+			currentSize += Time.deltaTime*2;
+			this.transform.localScale = new Vector3(currentSize, 1, 1);
+		}
+
+		if (currentPosition < finalPositon) {
+			currentPosition += Time.deltaTime;
+			this.transform.position = new Vector3 (currentPosition, this.transform.position.y, this.transform.position.z);
+		}
+	}
+}
