8c79d646c97dc5ed69040c3e987fc534d57201f3
YoHana19 <yohanashima@mail.com>
2017-12-03 16:48:24 +0900

    Tweak UI looks

---
+++ Assets/Scripts/AddCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCube : MonoBehaviour {
+
+	public GameObject cubeControllerUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddCubeContollerUI(){
+		GameObject cubeControllerUIObj = Instantiate (cubeControllerUI);
+		cubeControllerUIObj.transform.SetParent (this.transform, false);
+	}
+}
